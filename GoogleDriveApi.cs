64a02d1d70508f72ccfbcaa7a42f92eb328616c1
Truongphamm <truong20010619@gmai.com>
2024-05-12 21:54:20 +0700

    Viết API

--- GoogleDriveApi.cs
+++ GoogleDriveApi.cs
@@ -82,49 +82,49 @@ namespace UploadGoogleDrive
             return file.Id;
         }
 
-        //public async Task<string> UploadFileAsync(string filepath,
-        //                   CancellationToken cancel_token,
-        //                   string folderid = null,
-        //                   Action<double> callback_progressuploadchanged = null,
-        //                   string filename = null,
-        //                   string mimetype = "video/audio/image/jpeg",
-        //                   bool shareanyonewithlink = false)
-        //{
-        //    FileInfo fInfo = new FileInfo(filepath);
-        //    var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-        //    {
-        //        Name = filename == null ? fInfo.Name : filename,
-        //        Description = "Upload by tool DRADYO",
-        //        Parents = folderid != null ? new List<string> { folderid } : null
-        //    };
+        public async Task<string> UploadFileAsync(string filepath,
+                           CancellationToken cancel_token,
+                           string folderid = null,
+                           Action<double> callback_progressuploadchanged = null,
+                           string filename = null,
+                           string mimetype = "video/audio/image/jpeg",
+                           bool shareanyonewithlink = false)
+        {
+            FileInfo fInfo = new FileInfo(filepath);
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = filename == null ? fInfo.Name : filename,
+                Description = "Upload by tool DRADYO",
+                Parents = folderid != null ? new List<string> { folderid } : null
+            };
 
-        //    using (FileStream fs = new FileStream(filepath, FileMode.Open))
-        //    {
-        //        var requestUpload = _service.Files.Create(fileMetadata, fs, mimetype);
+            using (FileStream fs = new FileStream(filepath, FileMode.Open))
+            {
+                var requestUpload = _service.Files.Create(fileMetadata, fs, mimetype);
 
-        //        if (callback_progressuploadchanged != null)
-        //            requestUpload.ProgressChanged += (u) => callback_progressuploadchanged((double)u.BytesSent / fs.Length);
+                if (callback_progressuploadchanged != null)
+                    requestUpload.ProgressChanged += (u) => callback_progressuploadchanged((double)u.BytesSent / fs.Length);
 
-        //        var upload_response = await requestUpload.UploadAsync(cancel_token);
+                var upload_response = await requestUpload.UploadAsync(cancel_token);
 
-        //        if (upload_response.Exception != null)
-        //        {
-        //            throw upload_response.Exception;
-        //        }
+                if (upload_response.Exception != null)
+                {
+                    throw upload_response.Exception;
+                }
 
-        //        if (requestUpload.ResponseBody == null)
-        //            throw new Exception("Upload fail");
-        //        else
-        //        {
-        //            if (shareanyonewithlink)
-        //            {
-        //                string linkShare = await ShareAnyoneWithLinkAsync(_service, requestUpload.ResponseBody.Id);
-        //            }
+                if (requestUpload.ResponseBody == null)
+                    throw new Exception("Upload fail");
+                else
+                {
+                    if (shareanyonewithlink)
+                    {
+                        string linkShare = await ShareAnyoneWithLinkAsync(_service, requestUpload.ResponseBody.Id);
+                    }
 
-        //            return requestUpload.ResponseBody.Id;
-        //        }
-        //    }
-        //}
+                    return requestUpload.ResponseBody.Id;
+                }
+            }
+        }
 
 
         private async Task<string> ShareAnyoneWithLinkAsync(DriveService service, string fileid)
@@ -146,9 +146,9 @@ namespace UploadGoogleDrive
                 System.IO.File.Delete(fileSave);
         }
 
-        //public string GetLinkFile(string fileId)
-        //{
-        //    return $"https://drive.google.com/file/d/{fileId}";
-        //}
+        public string GetLinkFile(string fileId)
+        {
+            return $"https://drive.google.com/file/d/{fileId}";
+        }
     }
 }
